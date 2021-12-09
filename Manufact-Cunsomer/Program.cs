42ed92b71aded18ddcb63a8122eeb951b507b709
Anatoly <anatolypanushkin@mail.ru>
2021-12-09 13:21:37 +0300

    Smart queue add

--- Manufact-Cunsomer/Program.cs
+++ Manufact-Cunsomer/Program.cs
@@ -5,29 +5,23 @@ using System.Threading;
 namespace Manufacturers_Cunsomers
 {
     class Program
-    {
-        //исполнитель должен поождать
-        private static Mutex _cunsomerMustWait = new Mutex();
-        //производители должны подождать
-        private static Mutex _manufacturerMustWait = new Mutex();
-
-        private static Queue<Goals> buffer = new Queue<Goals>();
-        private static Goals _goals = new Goals();
-        private static int maxSize = 3;
-        
+    { 
         static void Main(string[] args)
         {
-
+            Queue manufacturer = new Queue();
+            Queue cunsomer = new Queue();
+            
+            
             Thread[] manufacturers = new Thread[3];
             for (int i = 0; i < 3; i++)
             {
-                manufacturers[i] = new Thread(CreateGoals);
+                manufacturers[i] = new Thread(manufacturer.AddGoals);
             }
 
             Thread[] cunsomers = new Thread[3];
             for (int i = 0; i < 3; i++)
             {
-                cunsomers[i] = new Thread(CompleteGoal);
+                cunsomers[i] = new Thread(cunsomer.CompleteGoal);
             }
 
             foreach (var v in manufacturers)
@@ -55,60 +49,7 @@ namespace Manufacturers_Cunsomers
             
             Console.ReadLine();
         }
+        
 
-        //Функция создания задачи для производителя
-        static void CreateGoals()
-        {
-            if (buffer.Count<maxSize)
-            {
-                _manufacturerMustWait.WaitOne();
-                //Добавляем новую задачу
-                buffer.Enqueue(_goals);
-                ConsoleHelper.WriteToConsole($"Manufacturer {Thread.CurrentThread.ManagedThreadId} add new goal");
-                Thread.Sleep(100);
-                _manufacturerMustWait.ReleaseMutex();
-                ManufacturerWait();
-            }
-            else
-            {
-                ManufacturerWait();
-            }
-        }
-
-        static void ManufacturerWait()
-        {
-            if (buffer.Count>=maxSize)
-                ConsoleHelper.WriteToConsole($"buffer is full!");
-            Thread.Sleep(100);
-            CreateGoals();
-        }
-
-
-        static void CompleteGoal()
-        {
-            
-            if (buffer.Count>0)
-            {
-                _cunsomerMustWait.WaitOne();
-                buffer.Dequeue();
-                ConsoleHelper.WriteToConsole($"Cunsomer {Thread.CurrentThread.ManagedThreadId} complete task");
-                Thread.Sleep(100);
-                _cunsomerMustWait.ReleaseMutex();
-                CunsomerWait();
-            }
-            else
-            {
-                CunsomerWait();
-            }
-        }
-
-        static void CunsomerWait()
-        {
-            
-            if (buffer.Count==0)
-                ConsoleHelper.WriteToConsole("buffer is empty!");
-            Thread.Sleep(100);
-            CompleteGoal();
-        }
     }
 }
---
+++ Manufact-Cunsomer/Queue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Manufacturers_Cunsomers
+{
+    public class Queue
+    {
+        private static Goals _goals = new Goals();
+        private static Queue<Goals> buffer = new Queue<Goals>();
+        private static int _maxSize = 5;
+        private static Mutex _mfcAndCmrWait = new Mutex();
+        private static Mutex _cunsomer = new Mutex();
+        
+        public void AddGoals()
+        {
+            _mfcAndCmrWait.WaitOne();
+            if (buffer.Count<_maxSize)
+            {
+                buffer.Enqueue(_goals);
+                Thread.Sleep(500);
+                ConsoleHelper.WriteToConsole($"Manufacture {Thread.CurrentThread.ManagedThreadId} add new goal");
+                _mfcAndCmrWait.ReleaseMutex();
+                ManufacturerWait();
+            }
+            else
+            {
+                ConsoleHelper.WriteToConsole($"buffer id full!");
+                _mfcAndCmrWait.ReleaseMutex();
+                ManufacturerWait();
+            }
+        }
+        
+        private void ManufacturerWait()
+        {
+            Thread.Sleep(100);
+            AddGoals();
+        }
+
+
+        public void CompleteGoal()
+        {
+            _mfcAndCmrWait.WaitOne();
+            if (buffer.Count>0)
+            {
+                buffer.Dequeue();
+                Thread.Sleep(500);
+                ConsoleHelper.WriteToConsole($"Cunsomer {Thread.CurrentThread.ManagedThreadId} complete goal");
+                _mfcAndCmrWait.ReleaseMutex();
+                CunsomerWait();
+            }
+            else
+            {
+                ConsoleHelper.WriteToConsole($"buffer is empty!");
+                _mfcAndCmrWait.ReleaseMutex();
+                CunsomerWait();
+            }
+        }
+        
+        private void CunsomerWait()
+        {
+            Thread.Sleep(100);
+            CompleteGoal();
+        }
+        
+    }
+}
